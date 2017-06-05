0fc424bc78e5e156cd512a6228147d1196419688
shinabarger <davidshinabarger@gmail.com>
2017-06-05 16:07:38 -0400

    add simple ajax request to wikipedia api and display a list of links based on the location offered.

--- js/script.js
+++ js/script.js
@@ -1,31 +1,48 @@
-
 function loadData() {
 
-    var $body = $('body');
-    var $wikiElem = $('#wikipedia-links');
-    var $nytHeaderElem = $('#nytimes-header');
-    var $nytElem = $('#nytimes-articles');
-    var $greeting = $('#greeting');
+  var $body = $('body');
+  var $wikiElem = $('#wikipedia-links');
+  var $nytHeaderElem = $('#nytimes-header');
+  var $nytElem = $('#nytimes-articles');
+  var $greeting = $('#greeting');
 
-    // clear out old data before new request
-    $wikiElem.text("");
-    $nytElem.text("");
+  // clear out old data before new request
+  $wikiElem.text("");
+  $nytElem.text("");
 
 
-    //This takes whatever the user puts into the text input id street and assigns it to a
-    //variable called StreetString, then just repeats the same thing again
-    var streetString = $('#street').val();
-    var cityString = $('#city').val();
-    var address = streetString + ', ' + cityString;
+  //This takes whatever the user puts into the text input id street and assigns it to a
+  //variable called StreetString, then just repeats the same thing again
+  var streetString = $('#street').val();
+  var cityString = $('#city').val();
+  var address = streetString + ', ' + cityString;
 
-    $greeting.text('So live there.');
+  $greeting.text('So live there.');
 
-    // load streetview
-    var streetViewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+  // load streetview
+  var streetViewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
 
-    $body.append('<img class="bmimg" src="' + streetViewUrl + '">');
+  $body.append('<img class="bmimg" src="' + streetViewUrl + '">');
 
-    return false;
+  var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityString + '&format=json&callback=wikiCallback';
+
+  $.ajax({
+    url: wikiUrl,
+    dataType: "jsonp",
+    success: function(response) {
+      var articleList = response[1];
+
+      for(var i = 0; i < articleList.length; i++) {
+        articleStr=articleList[i];
+        var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+        $wikiElem.append('<li><a href="' + url + '">' +
+        articleStr + '</a></li>');
+      };
+    }
+  })
+
+
+  return false;
 };
 
 $('#form-container').submit(loadData);
